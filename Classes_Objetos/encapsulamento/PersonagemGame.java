cf8718bc8374999b6f7203e6d20d17ddf779d7aa
Lucas Henrique Silva Gomes <lucas.lucantoni@gmail.com>
2025-07-13 02:55:26 +0000

    Encapsulamento Hoberton Bradesco

---
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -0,0 +1,23 @@
+public class PersonagemGame {
+    
+    private int saudeAtual; 
+    private String nome; 
+
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getSaudeAtual(){
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual){
+        this.saudeAtual = saudeAtual;
+    }
+
+}
