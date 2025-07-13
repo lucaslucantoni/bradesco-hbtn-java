49eab9f3423f6278b9cd9c49527b028324206e08
Lucas Henrique Silva Gomes <lucas.lucantoni@gmail.com>
2025-07-13 18:30:14 +0000

    Task

---
+++ Heranca/retangulo_completo/Retangulo.java
@@ -0,0 +1,41 @@
+public class Retangulo extends FormaGeometrica {
+
+    private double largura;
+    private double altura;
+
+    public double getLargura() {
+        return largura;
+    }
+
+    @Override
+    public String toString(){
+        return "[Retangulo] %s / %s".formatted(largura, altura);
+    }
+
+    @Override
+    public double area(){
+        return largura * altura;
+    }
+
+    public void setLargura(double largura) {
+
+        if (largura < 0){
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+
+        if (altura < 0){
+            throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+
+        this.altura = altura;
+    }
+    
+}
