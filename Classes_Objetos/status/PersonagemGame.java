d040ded5df0afb5d83ed5cf8426cc4846cae95fc
Lucas Henrique Silva Gomes <lucas.lucantoni@gmail.com>
2025-07-13 03:14:23 +0000

    First Commit

---
+++ Classes_Objetos/status/PersonagemGame.java
@@ -0,0 +1,45 @@
+public class PersonagemGame {
+    
+    private int saudeAtual; 
+    private String nome; 
+    private String status;
+
+
+    public void tomarDano(int quantidadeDeDano){
+
+        int newSaudeAtual = saudeAtual - quantidadeDeDano;
+        this.setSaudeAtual(newSaudeAtual < 0 ? 0 : newSaudeAtual);
+    }
+
+    public void receberCura (int quantidadeDeCura){
+
+        int newSaudeAtual = saudeAtual + quantidadeDeCura;
+        this.setSaudeAtual(newSaudeAtual > 100 ? 100 : newSaudeAtual);
+    }
+
+    public String getStatus(){
+        return this.status;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getSaudeAtual(){
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual){
+        if (saudeAtual > 0) {
+            status = "vivo";
+        } else if (saudeAtual == 0){
+            status = "morto";
+        }
+        this.saudeAtual = saudeAtual;
+    }
+
+}
