005ce1a541d641c525a3df72ec8fdf5061c6af73
Lucas Henrique Silva Gomes <lucas.lucantoni@gmail.com>
2025-07-13 18:01:27 +0000

    Forma

--- Heranca/forma/FormaGeometrica.java
+++ Heranca/forma/FormaGeometrica.java
@@ -1,7 +1,7 @@
 public class FormaGeometrica {
     
     public double area(){
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("Funcao area nao implementada");
     }
 
 }
