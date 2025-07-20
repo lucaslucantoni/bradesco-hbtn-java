d2d4ee031ce385adae05412b1131963e2ad7274f
Lucas Lucantoni <lucas.lucantoni@gmail.com>
2025-07-20 16:25:46 +0000

    Adding files

--- 0x03/livraria/Program.java
+++ 0x03/livraria/Program.java
@@ -1,58 +1,78 @@
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
+
         try {
-            ContaBancariaBasica conta1 = new ContaBancariaBasica("AAA", 50.0);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta1.getNumeracao(), conta1.getSaldo());
-            conta1.depositar(100);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta1.getNumeracao(), conta1.getSaldo());
-            conta1.sacar(25);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta1.getNumeracao(), conta1.getSaldo());
-            conta1.aplicarAtualizacaoMensal();
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n\n", conta1.getNumeracao(), conta1.getSaldo());
+            Livro livro1 = new Livro(
+                    "As barbas do imperador: D. Pedro II",
+                    "Lilia Moritz Schwarcz",
+                    56);
+
+            System.out.printf(new Locale("pt", "BR"),"Titulo: %s - Autor: %s - Preco: %.2f\n",
+                    livro1.getTitulo(),
+                    livro1.getAutor(),
+                    livro1.getPreco());
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
 
         try {
-            ContaBancariaBasica conta2 = new ContaBancariaBasica("BBB", 100.0);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta2.getNumeracao(), conta2.getSaldo());
-            conta2.depositar(10);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta2.getNumeracao(), conta2.getSaldo());
-            conta2.sacar(10);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta2.getNumeracao(), conta2.getSaldo());
-            conta2.aplicarAtualizacaoMensal();
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n\n", conta2.getNumeracao(), conta2.getSaldo());
+            LivroEdicaoOuro livroEdicaoOuro1 = new LivroEdicaoOuro(
+                    "Xato",
+                    "Fernando Morais",
+                    38.30);
+
+            System.out.printf(new Locale("pt", "BR"),"Titulo: %s - Autor: %s - Preco: %.2f\n",
+                    livroEdicaoOuro1.getTitulo(),
+                    livroEdicaoOuro1.getAutor(),
+                    livroEdicaoOuro1.getPreco());
+
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
 
         try {
-            ContaBancariaBasica conta3 = new ContaBancariaBasica("CCC", 120.0);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta3.getNumeracao(), conta3.getSaldo());
-            conta3.depositar(1600);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta3.getNumeracao(), conta3.getSaldo());
-            conta3.aplicarAtualizacaoMensal();
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n\n", conta3.getNumeracao(), conta3.getSaldo());
+            LivroEdicaoOuro livroEdicaoOuro2 = new LivroEdicaoOuro(
+                    "Ladrao de Raios",
+                    "Rick",
+                    21.88);
+
+            System.out.printf(new Locale("pt", "BR"),"Titulo: %s - Autor: %s - Preco: %.2f\n",
+                    livroEdicaoOuro2.getTitulo(),
+                    livroEdicaoOuro2.getAutor(),
+                    livroEdicaoOuro2.getPreco());
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
 
         try {
-            ContaBancariaBasica conta4 = new ContaBancariaBasica("DDD", 70.0);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta4.getNumeracao(), conta4.getSaldo());
-            conta4.depositar(0);
+            Livro livro2 = new Livro(
+                    "ab",
+                    "Jane Doe",
+                    15.99);
+
+            System.out.printf(new Locale("pt", "BR"),"Titulo: %s - Autor: %s - Preco: %.2f\n",
+                    livro2.getTitulo(),
+                    livro2.getAutor(),
+                    livro2.getPreco());
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
 
         try {
-            ContaBancariaBasica conta5 = new ContaBancariaBasica("EEE", 50.0);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta5.getNumeracao(), conta5.getSaldo());
-            conta5.depositar(25);
-            System.out.printf(new Locale("pt", "BR"),"Conta %s - saldo: %.2f\n", conta5.getNumeracao(), conta5.getSaldo());
-            conta5.sacar(30);
+            Livro livro3 = new Livro(
+                    "Harry Potter e o enigma do principe",
+                    "J.K. Rowling",
+                    0);
+
+            System.out.printf(new Locale("pt", "BR"),"Titulo: %s - Autor: %s - Preco: %.2f\n",
+                    livro3.getTitulo(),
+                    livro3.getAutor(),
+                    livro3.getPreco());
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
     }
+       
 }
