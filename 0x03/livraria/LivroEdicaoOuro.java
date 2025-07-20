c30c625bca0d8ada34dbeb37eb27fcea609cf1d8
Lucas Lucantoni <lucas.lucantoni@gmail.com>
2025-07-20 16:26:29 +0000

    Adding files

--- 0x03/livraria/LivroEdicaoOuro.java
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -1,7 +1,7 @@
 import exceptions.AutorInvalidoException;
 import exceptions.LivroInvalidoException;
 
-public class LivroEdicaoOuro extends Livro_ {
+public class LivroEdicaoOuro extends Livro {
     public LivroEdicaoOuro(String titulo, String autor, double preco) throws AutorInvalidoException, LivroInvalidoException {
         super(titulo, autor, preco);
     }
