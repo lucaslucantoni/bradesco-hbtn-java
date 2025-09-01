05ccad79257be99c6c230ba13e84b784fd3ee0af
Lucas Lucantoni <lucas.henrique.s.go@gmail.com>
2025-09-01 01:26:31 +0000

    Fixing files

--- Lambda_Exercicios/consulta_produtos/Program.java
+++ Lambda_Exercicios/consulta_produtos/Program.java
@@ -5,20 +5,20 @@ import java.util.Locale;
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
-        Produto produtoOld1 = new Produto_old("Cafe Chocolate Trufado Baggio",
-                21.99, 250, 120, TiposProduto_.COZINHA);
+        Produto produtoOld1 = new Produto("Cafe Chocolate Trufado Baggio",
+                21.99, 250, 120, TiposProduto.COZINHA);
 
-        Produto produtoOld2 = new Produto_old("Leite Condensado Moca",
-                6.29, 395, 200, TiposProduto_.COZINHA);
+        Produto produtoOld2 = new Produto("Leite Condensado Moca",
+                6.29, 395, 200, TiposProduto.COZINHA);
 
-        Produto produtoOld3 = new Produto_old("Carro Vortex",
-                799.90, 1560, 5, TiposProduto_.BRINQUEDO);
+        Produto produtoOld3 = new Produto("Carro Vortex",
+                799.90, 1560, 5, TiposProduto.BRINQUEDO);
 
-        Produto produtoOld4 = new Produto_old("Smart TV LED 32 HD LG",
-                1452.55, 6300, 7, TiposProduto_.ELETRONICO);
+        Produto produtoOld4 = new Produto("Smart TV LED 32 HD LG",
+                1452.55, 6300, 7, TiposProduto.ELETRONICO);
 
-        Produto produtoOld5 = new Produto_old("Nintendo Switch",
-                2990.00, 1700, 12, TiposProduto_.ELETRONICO);
+        Produto produtoOld5 = new Produto("Nintendo Switch",
+                2990.00, 1700, 12, TiposProduto.ELETRONICO);
 
         List<Produto> produtoOlds = Arrays.asList(
                 produtoOld1, produtoOld2, produtoOld3, produtoOld4, produtoOld5);
@@ -30,7 +30,7 @@ public class Program {
                 p -> p.getPeso() <= 1600);
 
         List<Produto> produtosCozinha = ConsultaProdutos.filtrar(produtoOlds,
-                p -> p.getTipo() == TiposProduto_.COZINHA);
+                p -> p.getTipo() == TiposProduto.COZINHA);
 
         List<Produto> produtosBaixoEstoque = ConsultaProdutos.filtrar(produtoOlds,
                 p -> p.getQuantidadeEmEstoque() <= 10);
