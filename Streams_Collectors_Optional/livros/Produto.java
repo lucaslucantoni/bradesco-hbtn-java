9cba22542172b6b7870df9e725aa84b34690ef0b
Lucas Lucantoni <lucas.henrique.s.go@gmail.com>
2025-09-01 01:52:27 +0000

    Files

--- Streams_Collectors_Optional/livros/Produto.java
+++ Streams_Collectors_Optional/livros/Produto.java
@@ -45,6 +45,7 @@ public class Produto {
 
     @Override
     public String toString() {
-        return String.format("[%d] %s %s R$ %,.2f", this.codigo, this.nome, this.categoria, this.preco);
+        String v = String.format("[%d] %s %s R$ %,.2f", this.codigo, this.nome, this.categoria, this.preco);
+        return v.replace(".", ",");
     }
 }
