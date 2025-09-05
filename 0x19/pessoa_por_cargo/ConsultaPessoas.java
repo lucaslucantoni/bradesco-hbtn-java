d9955f7ac515fd15b85e243b0b8065e54dca1608
Lucas Lucantoni <lucas.henrique.s.go@gmail.com>
2025-09-05 01:15:06 +0000

    Adding files

--- 0x19/pessoa_por_cargo/ConsultaPessoas.java
+++ 0x19/pessoa_por_cargo/ConsultaPessoas.java
@@ -3,10 +3,12 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ConsultaPessoas {
-
-    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> Pessoas) {
-        return Pessoas.stream().collect(
-                Collectors.groupingBy(Pessoa::getCargo)
-        );
+    private ConsultaPessoas() {
+    }
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas
+                .stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo))
+                ;
     }
 }
--- 0x19/pessoa_por_cargo/Pessoa.java
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -1,12 +1,13 @@
 import java.util.Locale;
 
 public class Pessoa {
-    
     private int codigo;
     private String nome;
     private String cargo;
     private int idade;
     private double salario;
+
+    public static final Locale LOCALE = new Locale("pt", "BR");
 
     public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
         this.codigo = codigo;
@@ -16,12 +17,28 @@ public class Pessoa {
         this.salario = salario;
     }
 
-    @Override
-    public String toString() {
-        return String.format(new Locale("pt", "BR"), "[%d] %s %s %d R$ %f", this.codigo, this.nome, this.cargo, this.idade, this.salario);
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
     }
 
     public String getCargo() {
         return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(LOCALE, "[%d] %s %s %d R$ %.6f", codigo, nome, cargo, idade, salario);
     }
 }
